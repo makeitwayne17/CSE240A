87d27f9f43c9bc8dcc72d5dc8460a04a9ee32cad
makeitwayne17 <bryantliu17@gmail.com>
2021-05-14 15:48:23 -0700

    auto commit

--- src/predictor.c
+++ src/predictor.c
@@ -6,8 +6,9 @@
 //  described in the README                               //
 //========================================================//
 #include <stdio.h>
+#include <stdint.h>
 #include "predictor.h"
-
+#define HIST_LEN 0xFFFFF
 //
 // TODO:Student Information
 //
@@ -16,6 +17,7 @@ const char *studentID = "A13458492";
 const char *email = "brl072@ucsd.edu";
 
 //Gshare hint: http://people.cs.pitt.edu/~childers/CS2410/slides/lect-branch-prediction.pdf
+//Gshare hint: https://www.youtube.com/watch?v=avp3bDqCXYM
 
 //------------------------------------//
 //      Predictor Configuration       //
@@ -38,6 +40,10 @@ int verbose;
 //
 //TODO: Add your own Branch Predictor data structures here
 //
+uint32_t gMask;
+uint32_t gHist;
+uint32_t gHistTable[HIST_LEN];
+// uint32_t gHistTable[UINTN_MAX];
 
 //------------------------------------//
 //        Predictor Functions         //
@@ -50,6 +56,19 @@ void init_predictor()
   //
   //TODO: Initialize Branch Predictor Data Structures
   //
+
+  // initialize gshare
+  gMask = 0;
+  gHist = 0;
+  for (int i = 0; i < ghistoryBits; i++) //setup the mask
+  {
+    gMask <<= 1;
+    gMask += 1;
+  }
+  for (int i = 0; i < HIST_LEN; i++)
+  {
+    gHistTable[i] = WN;
+  }
 }
 
 // Make a prediction for conditional branch instruction at PC 'pc'
@@ -69,6 +88,7 @@ make_prediction(uint32_t pc)
   case STATIC:
     return TAKEN;
   case GSHARE:
+    return gshare(pc);
   case TOURNAMENT:
   case CUSTOM:
   default:
@@ -85,7 +105,63 @@ make_prediction(uint32_t pc)
 //
 void train_predictor(uint32_t pc, uint8_t outcome)
 {
-  //
-  //TODO: Implement Predictor training
-  //
+  // Train a prediction based on the bpType
+  switch (bpType)
+  {
+  case STATIC:
+    break;
+  case GSHARE:
+    train_ghsare(pc, outcome);
+  case TOURNAMENT:
+  case CUSTOM:
+  default:
+    break;
+  }
+}
+
+// The Gshare predictor is characterized by XORing the global history register with the lower bits (same length as the global history) of the branch's address. This XORed value is then used to index into a 1D BHT of 2-bit predictors.
+//need a shift register of the most recent taken or not taken
+//combine global history with branch adress
+
+uint8_t
+gshare(uint32_t pc)
+{
+  uint32_t pcMasked = (pc & gMask);      //create a masked version of the PC counter
+  uint32_t histMasked = (gHist & gMask); //create a masked version of the history counter
+  uint32_t loc = pcMasked ^ histMasked;  //get the location to index into gHistTable
+  // fprintf(stderr, "loc: %ull        ghist: %ull\n", loc, gHistTable[loc]);
+
+  //return not taken if less than weakly taken (aka SN and WN)
+  if (gHistTable[loc] < WT)
+  {
+    return NOTTAKEN;
+  }
+  //return taken otherwise
+  return TAKEN;
+}
+
+//training of gShare
+void train_ghsare(uint32_t pc, uint8_t outcome)
+{
+  uint32_t pcMasked = (pc & gMask);      //create a masked version of the PC counter
+  uint32_t histMasked = (gHist & gMask); //create a masked version of the history counter
+  uint32_t loc = pcMasked ^ histMasked;  //get the location to index into gHistTable
+
+  if (outcome)
+  {
+    gHistTable[loc]--; //if outcome is not taken, we decrease gHist[i]
+  }
+  else
+  {
+    gHistTable[loc]++; //if outcome is taken, we increase gHist[i]
+  }
+  //bound check
+  if (gHistTable[loc] > ST)
+  {
+    gHistTable[loc] = ST;
+  }
+  else if (gHistTable[loc] < SN)
+  {
+    gHistTable[loc] = SN;
+  }
 }
