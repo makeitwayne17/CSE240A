1d6a23896ca082871172e5054ab77e81910b00e1
makeitwayne17 <bryantliu17@gmail.com>
2021-05-27 02:57:35 -0700

    auto commit

--- src/getResults.sh
+++ src/getResults.sh
@@ -4,12 +4,12 @@
 #     echo $filename
 # done
 
-for filename in ../traces/*.bz2; do
-    echo $filename;
-    bunzip2 -kc $filename| ./predictor --tournament:10:9:10;
-done
-
 # for filename in ../traces/*.bz2; do
 #     echo $filename;
-#     bunzip2 -kc $filename| ./predictor --custom:10:9:10;
+#     bunzip2 -kc $filename| ./predictor --tournament:10:9:10;
 # done
+
+for filename in ../traces/*.bz2; do
+    echo $filename;
+    bunzip2 -kc $filename| ./predictor --custom:10:9:10;
+done
