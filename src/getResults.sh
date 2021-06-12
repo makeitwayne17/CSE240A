b4c7f290341d62283d223a26c50468820e84684e
makeitwayne17 <bryantliu17@gmail.com>
2021-06-12 10:52:12 -0700

    auto commit

--- src/getResults.sh
+++ src/getResults.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 # for filename in ../traces/*.bz2; do
 #     bunzip2 -kc $filename| ./predictor --static;
-#     echo $filename
+#     # echo $filename
 # done
 
-echo -e "\n\nTournament:\n\n"
-for filename in ../traces/*.bz2; do
-    echo $filename;
-    bunzip2 -kc $filename| ./predictor --tournament:9:10:10;
-done
+# echo -e "\n\nTournament:\n\n"
+# for filename in ../traces/*.bz2; do
+#     # echo $filename;
+#     bunzip2 -kc $filename| ./predictor --tournament:10:13:13;
+# done
 
 # echo -e "\n\nCustom:\n\n"
 # for filename in ../traces/*.bz2; do
-#     echo $filename;
+#     # echo $filename;
 #     bunzip2 -kc $filename| ./predictor --custom:9:10:10;
 # done
 
 # echo -e '\n\nGSHARE:\n\n'
 # for filename in ../traces/*.bz2; do
-#     echo $filename;
-#     bunzip2 -kc $filename| ./predictor --gshare:10;
+#     # echo $filename;
+#     bunzip2 -kc $filename| ./predictor --gshare:13;
 # done
+for i in {9..14}
+do
+    echo -e "\n\nGSHARE:$i: \n\n"
+    for filename in ../traces/*.bz2; do
+        # echo $filename;
+        bunzip2 -kc $filename| ./predictor --gshare:i;
+    done
+done
